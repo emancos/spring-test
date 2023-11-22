a2b23f8de3f5256e27d549d55cae470a9950a5a2
Emanuel Costa <emancos@gmail.com>
2023-11-22 20:20:10 -0300

    feat: findByName method of the DepartmentRepository

--- src/main/java/com/github/springtest/repository/DepartmentRepository.java
+++ src/main/java/com/github/springtest/repository/DepartmentRepository.java
@@ -3,6 +3,9 @@ package com.github.springtest.repository;
 import com.github.springtest.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+    Optional<Department> findByName(String name);
 }
